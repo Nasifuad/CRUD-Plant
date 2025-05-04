6e3bb7a79c16813b29bb6d8e58dd2f82d4308436
Nasifuad <nasifuad007@gmail.com>
2025-05-04 20:09:12 +0600

    making auth routes

---
+++ src/modules/auth/auth.controller.js
@@ -0,0 +1,18 @@
+const login = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+const signup = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+const verifyEmail = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+const logout = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+export { login, signup, verifyEmail, logout };
--- src/modules/auth/auth.route.js
+++ src/modules/auth/auth.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-
+import { login, signup, verifyEmail, logout } from "./auth.controller.js";
 const router = Router();
 
-router.get("/test", (req, res) => {
-  res.send("Hello World!");
-});
+router.post("/login", login);
+router.post("/signup", signup);
+router.post("/verify-email", verifyEmail);
+router.post("/logout", logout);
 
 export const authRouter = router;
